1830bc28521674eef31fb0202d368aa1c940e728
Carlos Diaz <ca.diaz@hotmail.com>
2020-06-04 12:36:15 -0700

    stopwatchfase1finish

--- ios-clock/StopWatchViewController.swift
+++ ios-clock/StopWatchViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class StopWatchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
    
+    var laps : [String] = []
     var timer  = Timer()
     var minutes: Int = 0
     var seconds: Int = 0
@@ -28,7 +29,7 @@ class StopWatchViewController: UIViewController, UITableViewDelegate, UITableVie
     
     @IBAction func startStop(_ sender: Any) {
         if startStopWatch == true {
-            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: Selector(("updateStopwatch")), userInfo: nil, repeats: true)
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector:  #selector(self.updateStopwatch), userInfo: nil, repeats: true)
             
             startStopWatch = false
             startstopButton.setImage(UIImage(named: "StopButton.png"), for: UIControl.State.normal)
@@ -50,6 +51,24 @@ class StopWatchViewController: UIViewController, UITableViewDelegate, UITableVie
     
     
     @IBAction func lapReset(_ sender: Any) {
+        
+        if addLap == true  {
+            laps.insert(stopwatchString, at: 0)
+            lapsTableView.reloadData()
+            
+        }else {
+            addLap = false
+            lapresetButton.setImage(UIImage(named: "LapButton.png"), for: .normal)
+            laps.removeAll(keepingCapacity: false)
+            lapsTableView.reloadData()
+            
+            fractions = 0
+            seconds = 0
+            minutes = 0
+            stopwatchString = "00:00:00"
+            stopwatchLabel.text = stopwatchString
+            
+        }
     }
     
     override func viewDidLoad() {
@@ -58,7 +77,7 @@ class StopWatchViewController: UIViewController, UITableViewDelegate, UITableVie
         stopwatchLabel.text = "00.00.00"
     }
     
-    func updateStopwatch(){
+   @objc func updateStopwatch(){
         
         fractions += 1
         if fractions == 100 {
@@ -80,14 +99,14 @@ class StopWatchViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return 3
+        return laps.count
        }
        
        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell (style: UITableViewCell.CellStyle.value1, reuseIdentifier: "Cell")
         cell.backgroundColor = self.view.backgroundColor
-        cell.textLabel?.text = "Lap"
-        cell.detailTextLabel?.text = "00:00:0"
+        cell.textLabel?.text = "Lap \(laps.count-indexPath.row)"
+        cell.detailTextLabel?.text = laps[indexPath.row]
         
         return cell
         
